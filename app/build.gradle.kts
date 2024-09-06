ea01f0a79676a4fda03665d24c37d89d5961db89
fernandoneto03 <serafinineto@gmail.com>
2024-09-06 15:58:51 -0300

    Project Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.example.cameratest"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.cameratest"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("io.coil-kt:coil:2.0.0")
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.compose.material:material:1.5.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("io.coil-kt:coil-compose:2.0.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
+    implementation("androidx.core:core:1.12.0")
+}
---
+++ app/src/main/java/com/example/cameratest/MainActivity.kt
@@ -0,0 +1,290 @@
+package com.example.cameratest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
+import com.example.cameratest.ui.theme.CameraTestTheme
+import java.io.File
+
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CameraTestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppContent()
+                }
+            }
+        }
+    }
+}
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun AppContent(
+    uri: Uri? = null,
+    directory: File? = null,
+    onSetUri: (Uri) -> Unit = {}
+) {
+    val context = LocalContext.current
+    val tempUri = remember { mutableStateOf<Uri?>(null) }
+    val authority = stringResource(id = R.string.fileprovider)
+
+    // for takePhotoLauncher used
+    fun getTempUri(): Uri? {
+        directory?.let {
+            it.mkdirs()
+            val file = File.createTempFile(
+                "image_" + System.currentTimeMillis().toString(),
+                ".jpg",
+                File(it, "images")
+            )
+
+            return FileProvider.getUriForFile(
+                context,
+                authority,
+                file
+            )
+        }
+        return null
+    }
+
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = {
+            it?.let {
+                onSetUri.invoke(it)
+            }
+        }
+    )
+
+    val takePhotoLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicture(),
+        onResult = { _ ->
+            tempUri.value?.let {
+                onSetUri.invoke(it)
+            }
+        }
+    )
+
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // Permission is granted, launch takePhotoLauncher
+            val tmpUri = getTempUri()
+            tempUri.value = tmpUri
+            takePhotoLauncher.launch(tempUri.value)
+        } else {
+            // Permission is denied, handle it accordingly
+        }
+    }
+
+    var showBottomSheet by remember { mutableStateOf(false) }
+    if (showBottomSheet){
+        MyModalBottomSheet(
+            onDismiss = {
+                showBottomSheet = false
+            },
+            onTakePhotoClick = {
+                showBottomSheet = false
+
+                val permission = Manifest.permission.CAMERA
+                if (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED) {
+                    // Permission is already granted, proceed to step 2
+                    val tmpUri = getTempUri()
+                    if (tmpUri != null) {
+                        tempUri.value = tmpUri
+                        takePhotoLauncher.launch(tempUri.value)
+                    } else {
+                        Toast.makeText(context, "Failed to create file for photo", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // Permission is not granted, request it
+                    cameraPermissionLauncher.launch(permission)
+                }
+            },
+            onPhotoGalleryClick = {
+                showBottomSheet = false
+                imagePicker.launch(
+                    PickVisualMediaRequest(
+                        ActivityResultContracts.PickVisualMedia.ImageOnly
+                    )
+                )
+            },
+        )
+    }
+    Column (
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    showBottomSheet = true
+                }
+            ) {
+                Text(text = "Select / Take")
+            }
+        }
+
+        //preview selfie
+        uri?.let {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = it,
+                    modifier = Modifier.size(
+                        160.dp
+                    ),
+                    contentDescription = null,
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun MyModalBottomSheet(
+    onDismiss: () -> Unit,
+    onTakePhotoClick: () -> Unit,
+    onPhotoGalleryClick: () -> Unit
+) {
+    MyModalBottomSheetContent(
+        header = "Choose Option",
+        onDismiss = {
+            onDismiss.invoke()
+        },
+        items = listOf(
+            BottomSheetItem(
+                title = "Take Photo",
+                icon = Icons.Default.AccountBox,
+                onClick = {
+                    onTakePhotoClick.invoke()
+                }
+            ),
+            BottomSheetItem(
+                title = "select image",
+                icon = Icons.Default.Place,
+                onClick = {
+                    onPhotoGalleryClick.invoke()
+                }
+            ),
+        )
+    )
+}
+@RequiresApi(Build.VERSION_CODES.N)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyModalBottomSheetContent(
+    onDismiss: () -> Unit,
+    //header
+    header: String = "Choose Option",
+
+    items: List<BottomSheetItem> = listOf(),
+) {
+    val skipPartiallyExpanded by remember { mutableStateOf(false) }
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = skipPartiallyExpanded
+    )
+    val edgeToEdgeEnabled by remember { mutableStateOf(false) }
+    val windowInsets = if (edgeToEdgeEnabled)
+        WindowInsets(0) else BottomSheetDefaults.windowInsets
+
+    ModalBottomSheet(
+        shape = MaterialTheme.shapes.medium.copy(
+            bottomStart = CornerSize(0),
+            bottomEnd = CornerSize(0)
+        ),
+        onDismissRequest = { onDismiss.invoke() },
+        sheetState = bottomSheetState,
+        windowInsets = windowInsets
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                text = header,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center
+            )
+            items.forEach {item ->
+                androidx.compose.material3.ListItem(
+                    modifier = Modifier.clickable {
+                        item.onClick.invoke()
+                    },
+                    headlineContent = {
+                        Text(
+                            text = item.title,
+                            style = MaterialTheme.typography.titleMedium,
+                        )
+                    },
+                    leadingContent = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title
+                        )
+                    },
+                )
+            }
+        }
+    }
+}
+
+data class BottomSheetItem(
+    val title: String = "",
+    val icon: ImageVector,
+    val onClick: () -> Unit
+)
+
+
