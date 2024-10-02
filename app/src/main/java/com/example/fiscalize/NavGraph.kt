56e1fb8893e8e3e48b0839a3b315d5a1297b2c7a
fernandoneto03 <serafinineto@gmail.com>
2024-10-02 17:45:51 -0300

    feature Login Activity View

--- app/src/main/java/com/example/fiscalize/NavGraph.kt
+++ app/src/main/java/com/example/fiscalize/NavGraph.kt
@@ -10,16 +10,18 @@ import androidx.navigation.compose.rememberNavController
 import com.example.fiscalize.activities.CameraContent
 import com.example.fiscalize.activities.DashboardActivity
 import com.example.fiscalize.activities.HomeContent
+import com.example.fiscalize.activities.LoginActivity
 
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
 
-    NavHost( navController = navController, startDestination = "home", builder = {
+    NavHost( navController = navController, startDestination = "login", builder = {
         composable("home") { HomeContent(modifier,navController) }
         composable("dashboard") {  DashboardActivity(modifier,navController) }
         composable("camera") { CameraContent(modifier,navController) }
+        composable("login") { LoginActivity(modifier,navController) }
     })
 
 }
---
+++ app/src/main/java/com/example/fiscalize/activities/LoginActivity.kt
@@ -0,0 +1,63 @@
+package com.example.fiscalize.activities
+
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+
+@RequiresApi(Build.VERSION_CODES.N)
+@Composable
+fun LoginActivity(modifier: Modifier , navController: NavHostController) {
+
+    var login by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        OutlinedTextField(
+            value = login,
+            onValueChange = {login = it},
+            label = { Text("Login")},
+            modifier = Modifier.padding(6.dp)
+
+        )
+
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text("Senha")}
+        )
+
+        Button(
+            onClick = { Toast.makeText(context, "Logando usuario", Toast.LENGTH_SHORT).show() } ,
+            modifier = Modifier.padding(6.dp),
+        ) {
+            Text("Fazer login", color = Color.White)
+        }
+
+    }
+}
