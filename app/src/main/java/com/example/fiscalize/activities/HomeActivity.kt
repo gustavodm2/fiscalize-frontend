51feed6f2d208a59db9ebe6227f976b2938e244e
fernandoneto03 <serafinineto@gmail.com>
2024-10-08 18:42:24 -0300

    refactor: Login View Style

--- app/src/main/java/com/example/fiscalize/activities/HomeActivity.kt
+++ app/src/main/java/com/example/fiscalize/activities/HomeActivity.kt
@@ -14,30 +14,41 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
+import com.example.fiscalize.R
 import com.example.fiscalize.createImageFileUri
+import com.example.fiscalize.ui.theme.appGray
 import com.example.fiscalize.uploadImageToApi
 
 
@@ -93,7 +104,54 @@ fun HomeContent(
         Column(
             modifier = Modifier.fillMaxWidth(),
         ) {
-            Text(text = "Bem vindo, Roberto", fontSize = 20.sp, fontStyle = FontStyle.Italic)
+            Card(
+                modifier = Modifier
+                    .size(width = 380.dp, height = 100.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 8.dp),
+
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+                border = BorderStroke(1.dp, Color.Black)
+            ) {
+                Row(
+                    modifier = Modifier
+
+
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.logo),
+                        contentDescription = "App Logo",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .border(border = BorderStroke(1.dp, Color.Black), shape = RoundedCornerShape(50.dp))
+                            .size(88.dp)
+                            .clip(CircleShape),
+                        )
+                    Column(
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                    ){
+                        val corporateReason = "J&V"
+                        val CNPJ = "111.222.333-44"
+                        Text(
+                            text = "Razão Social: $corporateReason",
+                            fontSize = 20.sp,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            text = "CNPJ: $CNPJ",
+                            fontSize = 20.sp,
+                            fontStyle = FontStyle.Italic,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+
+            }
             Spacer(Modifier.padding(8.dp))
 
             LazyColumn(
