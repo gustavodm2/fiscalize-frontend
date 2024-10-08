de4e7dc24497e19be286542a1eb95f79d5af8cfa
gustavodm2 <gmerlugodenardi@gmail.com>
2024-10-08 11:43:52 -0300

    refactor: camera functions in cameraHandler now

--- app/src/main/java/com/example/fiscalize/activities/HomeActivity.kt
+++ app/src/main/java/com/example/fiscalize/activities/HomeActivity.kt
@@ -34,21 +34,11 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import coil.compose.rememberAsyncImagePainter
-import com.example.fiscalize.BuildConfig
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.MultipartBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.InputStream
-import java.text.SimpleDateFormat
-import java.util.*
+import com.example.fiscalize.createImageFileUri
+import com.example.fiscalize.uploadImageToApi
+
 
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
@@ -149,79 +139,4 @@ fun HomeContent(
     }
 }
 
-@SuppressLint("SimpleDateFormat")
-fun createImageFileUri(context: Context): Uri? {
-    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-    val imageFileName = "PNG_" + timeStamp + "_"
-    val storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-    if (storageDir != null && !storageDir.exists()) {
-        storageDir.mkdirs()
-    }
-    val imageFile = File.createTempFile(imageFileName, ".png", storageDir)
-    return FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", imageFile)
-}
 
-fun compressImage(inputStream: InputStream, maxSizeKB: Int): ByteArray {
-    val bitmap = BitmapFactory.decodeStream(inputStream)
-    val outputStream = ByteArrayOutputStream()
-
-    var quality = 100
-    do {
-        outputStream.reset()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
-        quality -= 10
-    } while (outputStream.size() / 1024 > maxSizeKB && quality > 0)
-
-    return outputStream.toByteArray()
-}
-
-
-fun uploadImageToApi(context: Context, imageUri: Uri) {
-    val contentResolver = context.contentResolver
-    val inputStream = contentResolver.openInputStream(imageUri)
-    val compressedImageBytes = compressImage(inputStream!!, 500)
-
-    val maxBytes = 5 * 1024 * 1024
-
-    val buffer = ByteArrayOutputStream()
-
-    try {
-        val byteArray = ByteArray(1024)
-        var totalBytesRead = 0
-        var len: Int
-
-        while (inputStream.read(byteArray).also { len = it } != -1 && totalBytesRead < maxBytes) {
-            if (totalBytesRead + len > maxBytes) {
-                len = maxBytes - totalBytesRead
-            }
-
-            buffer.write(byteArray, 0, len)
-            totalBytesRead += len
-        }
-
-
-
-        val requestBody = compressedImageBytes.toRequestBody("image/jpeg".toMediaTypeOrNull())
-        val multipartBody = MultipartBody.Part.createFormData("file", "image.png", requestBody)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val response = RetrofitInstance.api.uploadImage(multipartBody)
-
-                if (response.isSuccessful) {
-                    Log.d("UPLOAD", "Imagem enviada com sucesso: ${response}")
-
-                } else {
-                    Log.e("UPLOAD", "Erro no envio da imagem: ${response.errorBody()?.string()}")
-                }
-            } catch (e: Exception) {
-                Log.e("UPLOAD", "Falha na comunicação: ${e.message}")
-            }
-        }
-    } catch (e: Exception) {
-        Log.e("UPLOAD", "Erro ao processar imagem: ${e.message}")
-    } finally {
-        inputStream.close()
-        buffer.close()
-    }
-}
